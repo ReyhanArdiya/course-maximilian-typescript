85637243ad1dde84471a2947f83886b0f7fbc729
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-07-10 20:06:48 +0700

    Finish classes section

--- full-course-project/dist/app.js
+++ full-course-project/dist/app.js
@@ -1,19 +1,87 @@
 "use strict";
-const Button = (props) => {
-    const btn = document.createElement("button");
-    for (const prop in props) {
-        btn[prop] = props[prop];
+class Department {
+    name;
+    employees;
+    id;
+    constructor(name, employees = [], id = Math.random() * 2843 + "") {
+        this.name = name;
+        this.employees = employees;
+        this.id = id;
     }
-    return btn;
-};
-document.body.append(Button({
-    type: "submit",
-    innerText: "MEowwwHello!!!",
-    onclick() {
+    addEmployee(employee) {
+        this.employees.push(employee);
+    }
+    printEmployeesInfo() {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    }
+    static createEmployees(name) {
         console.log(this);
+        return [name + "1", name + "2"];
     }
-}));
-const a = (a) => a;
-const el = document.querySelector("meow");
-el?.addEventListener("fullscreenchange", a.bind(null));
+    static period = 2020;
+}
+class ITDepartment extends Department {
+    admins;
+    constructor(employees, admins, id = "ITdeptid") {
+        super("IT", employees, id);
+        this.admins = admins;
+    }
+    get adminsInfo() {
+        return this.admins.toString();
+    }
+    description() {
+        return "Mewo!";
+    }
+}
+class HRDept extends Department {
+    admins;
+    _lastReports;
+    static _instance = new this(["M<epw"], ["meow"]);
+    static get instance() {
+        return this._instance;
+    }
+    description() {
+        return "HRDept!";
+    }
+    constructor(employees, admins, _lastReports = [], id = "HRdeptid") {
+        super("HR", employees, id);
+        this.admins = admins;
+        this._lastReports = _lastReports;
+    }
+    get mostRecentReport() {
+        if (!this._lastReports.length) {
+            throw new Error("No reports!");
+        }
+        return this._lastReports.at(-1);
+    }
+    set mostRecentReport(report) {
+        if (!report)
+            throw new Error("No emptu report!");
+        this.addReports(report);
+    }
+    get lastReports() {
+        return this._lastReports;
+    }
+    set lastReports(value) {
+        this._lastReports = value;
+    }
+    addReports(...reports) {
+        this._lastReports.push(...reports);
+    }
+    removeReports() {
+        this._lastReports = [];
+    }
+    addEmployee(employee) {
+        if (employee === "Reyhan")
+            super.addEmployee(employee);
+    }
+}
+const itDept = new ITDepartment(["Reyhan", "Auren"], ["Argen"]);
+const hr1 = HRDept.instance;
+const hr2 = HRDept.instance;
+console.log(itDept);
+console.log(hr1, hr2);
+console.log(hr1 === hr2);
+console.log(HRDept.instance === HRDept.instance);
 //# sourceMappingURL=app.js.map
--- full-course-project/src/app.ts
+++ full-course-project/src/app.ts
@@ -1,25 +1,106 @@
-const Button = (props: object) => {
-	const btn = document.createElement("button");
-	for (const prop in props) {
-		//@ts-expect-error
-		btn[prop] = props[prop];
+abstract class Department {
+	constructor(
+		protected name: string,
+		protected employees: string[] = [],
+		readonly id: string = Math.random() * 2843 + ""
+	) {}
+
+	abstract description(): string;
+
+	addEmployee(employee: string) {
+		this.employees.push(employee);
 	}
 
-	return btn;
-};
+	printEmployeesInfo() {
+		console.log(this.employees.length);
+		console.log(this.employees);
+	}
 
-document.body.append(
-	Button({
-		type: "submit",
-		innerText: "MEowwwHello!!!",
-		onclick() {
-			console.log(this);
+	static createEmployees(name: string) {
+		console.log(this);
+		return [name + "1", name + "2"];
+	}
+
+	public static readonly period = 2020;
+}
+
+class ITDepartment extends Department {
+	constructor(
+		employees: Department["employees"],
+		protected admins: string[],
+		id: Department["id"] = "ITdeptid"
+	) {
+		super("IT", employees, id);
+	}
+
+	get adminsInfo() {
+		return this.admins.toString();
+	}
+
+	description(this: Department): string {
+		return "Mewo!";
+	}
+}
+
+class HRDept extends Department {
+	private static _instance: HRDept = new this(["M<epw"], ["meow"]);
+	static get instance() {
+		return this._instance;
+	}
+
+	description(): string {
+		return "HRDept!";
+	}
+
+	private constructor(
+		employees: Department["employees"],
+		protected admins: string[],
+		private _lastReports: string[] = [],
+		id: Department["id"] = "HRdeptid"
+	) {
+		super("HR", employees, id);
+	}
+
+	public get mostRecentReport() {
+		if (!this._lastReports.length) {
+			throw new Error("No reports!");
 		}
-	})
-);
 
-const a = <T extends Event>(a: T) => a;
+		return this._lastReports.at(-1)!;
+	}
 
-const el = document.querySelector("meow");
+	public set mostRecentReport(report: string) {
+		if (!report) throw new Error("No emptu report!");
 
-el?.addEventListener("fullscreenchange", a.bind(null));
+		this.addReports(report);
+	}
+
+	public get lastReports(): string[] {
+		return this._lastReports;
+	}
+	public set lastReports(value: string[]) {
+		this._lastReports = value;
+	}
+
+	addReports(...reports: typeof this._lastReports) {
+		this._lastReports.push(...reports);
+	}
+
+	removeReports() {
+		this._lastReports = [];
+	}
+
+	override addEmployee(employee: string): void {
+		// if (employee === "Reyhan") this.employees.push(employee);
+		if (employee === "Reyhan") super.addEmployee(employee);
+	}
+}
+
+const itDept = new ITDepartment(["Reyhan", "Auren"], ["Argen"]);
+const hr1 = HRDept.instance;
+const hr2 = HRDept.instance;
+console.log(itDept);
+console.log(hr1, hr2);
+
+console.log(hr1 === hr2);
+console.log(HRDept.instance === HRDept.instance);
