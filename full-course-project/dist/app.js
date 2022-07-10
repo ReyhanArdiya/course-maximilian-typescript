b543f491a1b4c8547c56b54946aa894dc4a08660
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2022-07-10 22:13:44 +0700

    Finish interfaces

--- full-course-project/dist/app.js
+++ full-course-project/dist/app.js
@@ -1,87 +1,30 @@
 "use strict";
-class Department {
+class Person {
     name;
-    employees;
-    id;
-    constructor(name, employees = [], id = Math.random() * 2843 + "") {
+    age;
+    constructor(name, age) {
         this.name = name;
-        this.employees = employees;
-        this.id = id;
+        this.age = age;
     }
-    addEmployee(employee) {
-        this.employees.push(employee);
-    }
-    printEmployeesInfo() {
-        console.log(this.employees.length);
-        console.log(this.employees);
-    }
-    static createEmployees(name) {
-        console.log(this);
-        return [name + "1", name + "2"];
-    }
-    static period = 2020;
-}
-class ITDepartment extends Department {
-    admins;
-    constructor(employees, admins, id = "ITdeptid") {
-        super("IT", employees, id);
-        this.admins = admins;
-    }
-    get adminsInfo() {
-        return this.admins.toString();
-    }
-    description() {
-        return "Mewo!";
+    greet() {
+        return `Hello! I am ${this.name} and ${this.age} old`;
     }
 }
-class HRDept extends Department {
-    admins;
-    _lastReports;
-    static _instance = new this(["M<epw"], ["meow"]);
-    static get instance() {
-        return this._instance;
+const person = new Person("Max", 20);
+let addFn;
+addFn = (a, b) => a + b;
+const str = addFn("Meow", "mere");
+class Product {
+    name;
+    alternateName;
+    constructor(name, alternateName) {
+        this.name = name;
+        this.alternateName = alternateName;
     }
-    description() {
-        return "HRDept!";
-    }
-    constructor(employees, admins, _lastReports = [], id = "HRdeptid") {
-        super("HR", employees, id);
-        this.admins = admins;
-        this._lastReports = _lastReports;
-    }
-    get mostRecentReport() {
-        if (!this._lastReports.length) {
-            throw new Error("No reports!");
-        }
-        return this._lastReports.at(-1);
-    }
-    set mostRecentReport(report) {
-        if (!report)
-            throw new Error("No emptu report!");
-        this.addReports(report);
-    }
-    get lastReports() {
-        return this._lastReports;
-    }
-    set lastReports(value) {
-        this._lastReports = value;
-    }
-    addReports(...reports) {
-        this._lastReports.push(...reports);
-    }
-    removeReports() {
-        this._lastReports = [];
-    }
-    addEmployee(employee) {
-        if (employee === "Reyhan")
-            super.addEmployee(employee);
+    printAlternateName() {
+        console.log(this.alternateName);
     }
 }
-const itDept = new ITDepartment(["Reyhan", "Auren"], ["Argen"]);
-const hr1 = HRDept.instance;
-const hr2 = HRDept.instance;
-console.log(itDept);
-console.log(hr1, hr2);
-console.log(hr1 === hr2);
-console.log(HRDept.instance === HRDept.instance);
+const book = new Product("Hello!");
+book.printAlternateName();
 //# sourceMappingURL=app.js.map
---
+++ full-course-project/src/interfaces.ts
@@ -0,0 +1,47 @@
+interface Named {
+	readonly name: string;
+}
+
+interface Greetable extends Named {
+	greet(): string;
+}
+
+class Person implements Greetable {
+	constructor(public name: string, public age: number) {}
+
+	greet(): string {
+		return `Hello! I am ${this.name} and ${this.age} old`;
+	}
+}
+
+const person: Greetable = new Person("Max", 20);
+
+// console.log(person.greet());
+
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+	// (a: number, b: number): number;
+	(a: string, b: string): string;
+}
+
+let addFn: AddFn;
+
+addFn = (a, b) => a + b;
+
+const str = addFn("Meow", "mere");
+
+interface Sellable extends Named {
+	alternateName?: string;
+}
+
+class Product implements Sellable {
+	constructor(public name: string, public alternateName?: string) {}
+
+	public printAlternateName() {
+		console.log(this.alternateName);
+	}
+}
+
+const book = new Product("Hello!");
+
+book.printAlternateName();
